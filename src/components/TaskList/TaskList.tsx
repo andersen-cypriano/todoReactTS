0f4a60314f9aa1a05acb7a7691d1e56ad5537793
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-08-07 12:33:56 -0300

    build

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,16 +1,11 @@
 import { ITask } from "../../interfaces/ITask";
-<<<<<<< HEAD
 import  styles  from "./TaskList.module.css";
-=======
-
->>>>>>> 68edf6b1897ee9b47d664c5ad7a07c3981703e2c
 interface Props {
   taskList: ITask[]
 }
 
 const TaskList = ({taskList}: Props) => {
   return (
-<<<<<<< HEAD
     <div className={styles.taskListContent}>
       <h1>Lista de Tarefas</h1>
       {taskList.length > 0 ? (
@@ -27,21 +22,6 @@ const TaskList = ({taskList}: Props) => {
         ) : (
           <p className={styles.noItem}>Sem tarefas <b>:(</b></p>
         )}
-=======
-    <div className="taskList-content">
-      <ul>
-        {taskList.length > 0 ? (
-          taskList.map(task => (
-            <li key={task.id}>
-              <h1 className="title">{task.title}</h1>
-              <p className="">{task.description}</p>
-            </li>
-          ))
-        ) : (
-          <p>Sem tarefas</p>
-        )}
-      </ul>
->>>>>>> 68edf6b1897ee9b47d664c5ad7a07c3981703e2c
     </div>
   )
 }
